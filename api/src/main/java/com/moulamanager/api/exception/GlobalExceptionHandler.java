a8736c4a3c145ceaa40777a8e6d8ae62aacd7812
Jaris <john.aristosa@hotmail.com>
2023-11-20 21:06:16 +0100

    update(exception): Update Global exception handling
    - Update global exception handling with private function that builds body and response

--- api/src/main/java/com/moulamanager/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/moulamanager/api/exception/GlobalExceptionHandler.java
@@ -7,28 +7,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = ProductNotFoundException.class)
     public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException exception) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+        return buildResponseException(exception, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = ProductAlreadyExistsException.class)
     public ResponseEntity<Object> handleProductAlreadyExistsException(ProductAlreadyExistsException exception) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(exception.getMessage());
+        return buildResponseException(exception, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+        return buildResponseException(exception, HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
+
+    private static ResponseEntity<Object> buildResponseException(RuntimeException exception, HttpStatus httpStatus) {
+        Map<String, Object> body = buildBodyExceptionResponse(exception);
+        return ResponseEntity
+                .status(httpStatus)
+                .body(body);
+    }
+
+    private static Map<String, Object> buildBodyExceptionResponse(Exception exception) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("message", exception.getMessage());
+        return body;
+    }
+
+
+
 }
