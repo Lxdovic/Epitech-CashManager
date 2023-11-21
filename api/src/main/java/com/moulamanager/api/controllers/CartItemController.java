d574f1ab99de706b7e9a91adcfc2c4c3f72e5176
Jaris <john.aristosa@hotmail.com>
2023-11-21 20:28:28 +0100

    chore(cartItem): Create cartItem controller

---
+++ api/src/main/java/com/moulamanager/api/controllers/CartItemController.java
@@ -0,0 +1,23 @@
+package com.moulamanager.api.controllers;
+
+import com.moulamanager.api.models.CartItemModel;
+import com.moulamanager.api.services.cartItem.CartItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carts/items")
+public class CartItemController {
+
+    private final CartItemService cartItemService;
+
+    public CartItemController(CartItemService cartItemService) {
+        this.cartItemService = cartItemService;
+    }
+
+
+}
