b59c87e29e3d488f3911a0b47443b23d058af476
Jaris <john.aristosa@hotmail.com>
2023-11-21 11:59:47 +0100

    chore(cart): Create cart controller

---
+++ api/src/main/java/com/moulamanager/api/controllers/CartController.java
@@ -0,0 +1,23 @@
+package com.moulamanager.api.controllers;
+
+import com.moulamanager.api.models.CartModel;
+import com.moulamanager.api.services.cart.CartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    
+}
