0ad327e6bc9aab3a48ed26e71060dc6a5b0958a6
Jaris <john.aristosa@hotmail.com>
2023-12-19 13:35:14 +0100

    rename(api): rename ApiURL to ApiEndpoints

---
+++ client/app/src/main/java/com/example/moulamanagerclient/data/repositories/ApiEndpoints.kt
@@ -0,0 +1,5 @@
+package com.example.moulamanagerclient.data.repositories
+
+object ApiEndpoints {
+    const val BASE_URL = "http://10.0.2.2:8080"
+}
--- client/app/src/main/java/com/example/moulamanagerclient/utils/Retrofit.kt
+++ client/app/src/main/java/com/example/moulamanagerclient/utils/Retrofit.kt
@@ -1,9 +1,9 @@
 package com.example.moulamanagerclient.utils
 
 import okhttp3.logging.HttpLoggingInterceptor
+import com.example.moulamanagerclient.data.repositories.ApiEndpoints
 import com.example.moulamanagerclient.data.repositories.ApiHeader
 import com.example.moulamanagerclient.data.repositories.ApiService
-import com.example.moulamanagerclient.data.repositories.ApiURL
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -30,7 +30,7 @@ object Retrofit {
 
     private val client = builder.build()
     private val retrofit = Retrofit.Builder()
-        .baseUrl(ApiURL.BASE_URL)
+        .baseUrl(ApiEndpoints.BASE_URL)
         .client(client)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
