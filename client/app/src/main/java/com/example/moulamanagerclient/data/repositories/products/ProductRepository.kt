12b9604c9261a51217215a03ce71ed05c8c38cb3
Jaris <john.aristosa@hotmail.com>
2023-12-26 23:59:16 +0100

    chore(product): create API call to get products like "name"

--- client/app/src/main/java/com/example/moulamanagerclient/data/repositories/products/ProductRepository.kt
+++ client/app/src/main/java/com/example/moulamanagerclient/data/repositories/products/ProductRepository.kt
@@ -7,18 +7,55 @@ import com.example.moulamanagerclient.data.network.ApiResult
 import com.example.moulamanagerclient.data.network.ApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class ProductRepository(
+class ProductRepository
+@Inject
+constructor(
 	private val apiService: ApiService
 ) {
+	private val productCache = mutableMapOf<String, Pagination<ProductResponse>>()
 
-	suspend fun getProducts(page: Int): ApiResult<Pagination<ProductResponse>> {
+	suspend fun getProducts(page: Int, size: Int): ApiResult<Pagination<ProductResponse>> {
 		return withContext(Dispatchers.IO) {
-			handleApiResponse(
-				request = { apiService.getProducts(page) }
+			val cacheKey = "products_page_$page"
+			val cachedProduct = productCache[cacheKey]
+			if (cachedProduct != null) {
+				return@withContext ApiResult.Success(cachedProduct)
+			}
+
+			val result = handleApiResponse(
+				request = { apiService.getProducts(page, size) }
 			)
+
+			if (result is ApiResult.Success && result.data != null) {
+				productCache[cacheKey] = result.data
+			}
+
+			result
 		}
 	}
+
+	suspend fun searchProductsByName(name: String, page: Int, size: Int): ApiResult<Pagination<ProductResponse>> {
+		return withContext(Dispatchers.IO) {
+			val cacheKey = "search_${name}_page_$page"
+			val cachedProduct = productCache[cacheKey]
+			if (cachedProduct != null) {
+				return@withContext ApiResult.Success(cachedProduct)
+			}
+
+			val result = handleApiResponse(
+				request = { apiService.searchProductsByName(name, page, size) }
+			)
+
+			if (result is ApiResult.Success && result.data != null) {
+				productCache[cacheKey] = result.data
+			}
+
+			result
+		}
+	}
+
 	suspend fun getProductByBarcode(barcode: String): ApiResult<ProductResponse> {
 		return withContext(Dispatchers.IO) {
 			handleApiResponse(
